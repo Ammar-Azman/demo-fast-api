d25b365722b43a753cd01590767c62f0b118e2b3
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-03 14:13:06 +0800

    add new models

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Header
 from fastapi.responses import JSONResponse
-from routers import users
+from routers import users, login
 
-desc="""
+desc = """
 simple-user-db-API
 ## Users
 
@@ -13,31 +13,27 @@ You will be able to
 * **Update User**
 """
 app = FastAPI(
-    title="user-db-API", 
+    title="user-db-API",
     description=desc,
-    version="0.0.1", 
-    contact={
-        "name":"Ammar-Azman", 
-        "url":"https://github.com/Ammar-Azman"
-    }, 
+    version="0.0.1",
+    contact={"name": "Ammar-Azman", "url": "https://github.com/Ammar-Azman"},
 )
 app.include_router(users.router)
+app.include_router(login.router)
+
 
 @app.get("/")
 def root():
-    return {
-        "Health":"Good!", 
-        "CI/CD":"Succed!"
-    }
+    return {"Health": "Good!", "CI/CD": "Succed!"}
+
 
 @app.get("/header")
 def read_header(user_agent=Header(None)):
     """
-    NOTE: Not understand about 
-    why it supposed to be underscore 
+    NOTE: Not understand about
+    why it supposed to be underscore
     instead of hypen
     """
     print(user_agent)
-    content = {"message":"hello-world!"}
-    return {"content":content, 
-        "User-Agent":user_agent}
+    content = {"message": "hello-world!"}
+    return {"content": content, "User-Agent": user_agent}
--- models.py
+++ models.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel, Field
-from typing_extensions import Annotated
+from pydantic import BaseModel
+from typing import Union, Annotated
 from fastapi import Query
 from enum import Enum
 
 
 class UserName(str, Enum):
-    user:str= "ammar"
-    mainUser:str="sara"
-    superUser:str="joe"
+    user: str = "ammar"
+    mainUser: str = "sara"
+    superUser: str = "joe"
+
 
 class UserDetails(BaseModel):
     fullname: Annotated[str, Query(max_length=10)]
@@ -15,9 +16,19 @@ class UserDetails(BaseModel):
     job: Annotated[str, Query(max_length=20)]
 
     class Config:
-        schema_extra = {
-                "fullname":"Leya", 
-                "age":21, 
-                "job":"tko"
-        }
-    
+        schema_extra = {"fullname": "Leya", "age": 21, "job": "tko"}
+
+
+class UserInput(BaseModel):
+    username: Annotated[str, Query(min_length=5, max_length=10)] = "Bryan"
+    password: Annotated[str, Query(min_length=5, max_length=10)] = "pasw!023"
+    fullname: Union[str, None] = None
+
+
+class UserOutput(BaseModel):
+    """
+    No output returned
+    """
+
+    username: Annotated[str, Query(min_length=5, max_length=10)]
+    fullname: Union[str, None] = None
