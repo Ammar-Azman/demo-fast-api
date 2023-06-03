eb195587ee94d93a189aee3dd06b5a66aab9a00e
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-03 19:09:08 +0800

    added errorhandling to upload endp

--- routers/upload.py
+++ routers/upload.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, File, UploadFile
-from typing import Annotated
+from fastapi import APIRouter, UploadFile, HTTPException, status
 
 router = APIRouter(prefix="/upload", tags=["upload"])
 
 
 @router.post("/file")
 async def upload_file(file: UploadFile):
+    if not file:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No file uploaded"
+        )
     return {
         "filename": file.filename,
         "content-type": file.content_type,
