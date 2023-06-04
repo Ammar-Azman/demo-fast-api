7a6493e38d7db41fc86b20a25380e67d9d374e20
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-04 13:55:35 +0800

    remove get active branch

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ You will be able to
 * **Update User**
 """
 
+# NOTE: KIV
+# def get_active_branch_name():
+#     head_dir = Path(".") / ".git" / "HEAD"
+#     with head_dir.open("r") as f:
+#         content = f.read().splitlines()
 
-def get_active_branch_name():
-    head_dir = Path(".") / ".git" / "HEAD"
-    with head_dir.open("r") as f:
-        content = f.read().splitlines()
-
-    for line in content:
-        if line[0:4] == "ref:":
-            return line.partition("refs/heads/")[2]
+#     for line in content:
+#         if line[0:4] == "ref:":
+#             return line.partition("refs/heads/")[2]
+# git_branch = get_active_branch_name()
 
 
-git_branch = get_active_branch_name()
 app = FastAPI(
-    title=f"user-db-API-{git_branch}",
+    title="user-db-API",
     description=desc,
     version="0.0.1",
     contact={"name": "Ammar-Azman", "url": "https://github.com/Ammar-Azman"},
