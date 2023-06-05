666d4cc134ecdda744cb27d38deef9f48fce9b39
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-05 11:43:49 +0800

    update /token

--- routers/security.py
+++ routers/security.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from models import UserInSystem, UserInCompany
 from db import fakeDB
+from datetime import datetime, timedelta
 from dependencies.security_dep import (
     fake_hash_password,
     get_current_user,
     get_current_active_user,
+    authenticate_user,
+    create_access_token,
 )
+from config.security_conf import TokenConfig
 
 
 db_user = fakeDB.fake_user_db
@@ -23,6 +27,8 @@ async def read_user_me(
 
 @router.post("/")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    """
+    # OLDER VERSION
     user_dict = db_user.get(form_data.username)  # retrieved from database
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
@@ -34,4 +40,27 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     # pwd validation
     if not hashed_password == user.hashed_password:
         raise HTTPException(status_code=400, details="Incorrect username or password")
-    return {"access_token": user.username, "token_type": "bearer"}
+    """
+    # NOTE: Validation occured here where hashed_pwd from database is compare
+    # with input password from user that will be hashed
+    # hashed(user_pwd) == hashed_pwd_db
+    user = authenticate_user(
+        fake_db=db_user, username=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # NOTE: Token expires time for authenticated user
+    access_token_expires = timedelta(minutes=TokenConfig.ACCESS_TOKEN_EXPIRE_MINUTE)
+
+    # note: the sub, access_token, token_type is constant (cannot customize name)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
