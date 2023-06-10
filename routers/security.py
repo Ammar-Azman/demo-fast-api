33928cbb9be757ea1806e865f06d8b566f739e86
Ammar-Azman <muhammarsafuan@gmail.com>
2023-06-10 15:51:28 +0800

    added root_path, subapi

--- routers/security.py
+++ routers/security.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from models import UserInSystem, UserInCompany
